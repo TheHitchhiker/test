6fcf98d972a283a95fd4c7c98cb871b7bd04e001
TheHitchhiker <heni.amiri@gmail.com>
2019-02-05 14:20:05 +0100

    removed comments from hook script

--- config/hooks/pre-commit.git-crypt.sh
+++ config/hooks/pre-commit.git-crypt.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-################################################################################
-# See <https://gist.github.com/Falkor/848c82daa63710b6c132bb42029b30ef>
-# Pre-commit hook to avoid accidentally adding unencrypted files with [git-crypt](https://www.agwa.name/projects/git-crypt/)
-# Fix to [Issue #45](https://github.com/AGWA/git-crypt/issues/45)
-#
-# Usage: 
-#    $> cd /path/to/repository
-#    $> git-crypt init
-#    $> curl <url/to/this/raw/gist> -o .git/hooks/pre-commit
-#    $> chmod +x .git/hooks/pre-commit
-#
-# Otherwise, you might want to add it as a git submodule, using:
-#    $> git submodule add https://gist.github.com/848c82daa63710b6c132bb42029b30ef.git config/hooks/pre-commit.git-crypt
-#    $> cd .git/hooks
-#    $> ln -s ../../config/hooks/pre-commit.git-crypt/pre-commit.git-crypt.sh pre-commit
-#
-
 if [ -d .git-crypt ]; then
     STAGED_FILES=$(git diff --cached --name-status | awk '$1 != "D" { print $2 }' | xargs echo)
     if [ -n "${STAGED_FILES}" ]; then
